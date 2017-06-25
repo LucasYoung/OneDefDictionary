bbafb6640ae7554ee885b1438a25bb85ff6d67d9
Thomas Hui Li <thomasli537@gmail.com>
2017-06-24 20:43:08 -0700

    second commit

--- OneDefDictionary.py
+++ OneDefDictionary.py
@@ -1,1 +1,14 @@
 import nltk
+
+class node(word, definition):
+	def __init__(self, word, )
+	array_of_nodes = []
+
+	# each node has a set of words aka the definition
+	# each node will have a counter
+	# walk through array of nodes comparing words in set to words in context
+	#  
+
+
+
+class definition(set_of_words,)
