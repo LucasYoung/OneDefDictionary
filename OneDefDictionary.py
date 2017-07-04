fff320469451fa62e7f33cb7eeed675ffb5619e1
LucasYoung <ly.youngs@gmail.com>
2017-07-04 03:54:52 -0400

    Created dictionary class. Refactored filtering stop words.

--- OneDefDictionary.py
+++ OneDefDictionary.py
@@ -4,36 +4,64 @@ from nltk.tokenize import word_tokenize
 
 stop_words = set(stopwords.words("english"))
 
-oneDefDict = {}
+def filterStopWords(sentence):
+	words = word_tokenize(sentence)
+	filtered_words = {}
+	for w in words:
+		if w not in stop_words:
+			filtered_words.append(w)
+	return filtered_words
 
-def getDefinition(word):
-	node = 
-	return node.definition
+class Dictionary(object):
+	def __init__(self):
+		dictionary = {}
 
+	# adds a node to the dictionary
+	def addWord(self, word, definition):
+		newNode = Node(word, definition)
+
+		if dict.get(word) is None:
+			self.dictionary[word] = [newNode]
+		else:
+			self.dictionary[word].append(newNode)
+
+	# calculates score for how well a node matches a context
+	# score is the number of matches between frequency dict and words in context
+	def judge(contextWordSet, freq_dict):
+		score = 0
+		for word in contextWordSet:
+			frequency = freq_dict.get(word)
+			if frequency is not None:
+				score+=frequency
+		return score
+
+	# gets the appropriate definition of a word and context
+	def getDefinition(self, word, context):
+		contextWordSet = filterStopWords(context)
+
+		maxScore = 0
+		maxNode = None
+		for node in self.dictionary[word]:
+			score = self.judge(contextWordSet, node.freq_dict)
+			if score > maxScore:
+				maxScore = score
+				maxNode = node
+
+		if(maxNode is not None):
+			return maxNode
+		else:
+			return None
 
 class Node(object):
 
 	# needs modifying for actual texts
 	def freq_dict_initializer(definition):
-		words = word_tokenize(definition)
-		filtered_words = []
-		for w in words:
-			if w not in stop_words:
-				filtered_words.append(w)
-		freq_dictionary = dict.fromkeys(filtered_words, 0)
-		return freq_dictionary
+		return dict.fromkeys(filterStopWords(definition), 0)
 
 	def __init__(self, word, definition):
-		self.freq_dict = freq_dict_initializer(definition)
+		self.freq_dict = self.freq_dict_initializer(definition)
 		self.word = word
 		self.definition = definition
-
-
-
-
-
-
-	
 
 	# each node has a set of words aka the definition
 	# each node will have a counter
@@ -44,7 +72,3 @@ class Node(object):
 	#
 	# 
 	#
-
-
-
-
